c3377bd5337fcf9de42ca24d08323d193ad9a54f
MAGG0059 <manuel.guarnizo-g@mail.escuelaing.edu.co>
2025-09-24 19:56:33 -0500

    feat: "Project Structure DTO"

--- src/main/java/edu/dosw/model/Task.java
+++ src/main/java/edu/dosw/model/Task.java
@@ -5,12 +5,12 @@ import java.time.LocalDateTime;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-
 @Document(collection = "Tasks")
 public class Task {
     @Id
-    private String title;
+    private String id;
 
+    private String title;
     private String description;
     private LocalDateTime date;
     private States state;
@@ -22,15 +22,15 @@ public class Task {
         this.state = state;
     }
 
+    public String getId() { return id; }
     public String getTitle() { return title; }
     public String getDescription() { return description; }
     public LocalDateTime getDate() { return date; }
     public States getState() { return state; }
+
+    public void setId(String id) { this.id = id; }
+    public void setTitle(String title) { this.title = title; }
+    public void setDescription(String description) { this.description = description; }
+    public void setDate(LocalDateTime date) { this.date = date; }
     public void setState(States state) { this.state = state; }
-
-    @Override
-    public String toString() {
-        return title + " [" + state + "]";
-    }
-
 }
