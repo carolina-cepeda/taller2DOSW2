099f4bc913a70c6861450cbd8e243f07a394aef2
MAGG0059 <manuel.guarnizo-g@mail.escuelaing.edu.co>
2025-09-28 09:44:39 -0500

    feature: "User Controller"

--- src/main/java/edu/dosw/controller/UserController.java
+++ src/main/java/edu/dosw/controller/UserController.java
@@ -2,54 +2,25 @@ package edu.dosw.controller;
 
 import edu.dosw.model.User;
 import edu.dosw.model.UserType;
-import edu.dosw.services.UserFactory;
-import edu.dosw.services.TaskService;
-import edu.dosw.model.Task;
-import edu.dosw.dto.TaskDTO;
+import edu.dosw.services.UserService;
 
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
 
-    private final UserFactory userFactory;
-    private final TaskService taskService;
+    private final UserService userService;
 
     @Autowired
-    public UserController(UserFactory userFactory, TaskService taskService) {
-        this.userFactory = userFactory;
-        this.taskService = taskService;
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
     @PostMapping("/create")
     public User createUser(@RequestParam String id,@RequestParam String username, @RequestParam UserType type) {
-        return userFactory.generateUser(id,username, type);
-    }
-
-    @PostMapping("/{userType}/task")
-    public Task createTask(@PathVariable UserType userType, @RequestBody TaskDTO task) {
-        if (userType == UserType.ADMIN || userType == UserType.MEMBER) {
-            return taskService.createTask(task);
-        }
-        throw new RuntimeException("No tienes permisos para crear tareas");
-    }
-
-    @GetMapping("/{userType}/tasks")
-    public List<Task> getTasks(@PathVariable UserType userType) {
-        return taskService.getTasks();
-    }
-
-    @GetMapping("/{userType}/tasks/{filter}/{extra}")
-    public List<Task> getTasksByFilter(@PathVariable UserType userType,
-                                       @PathVariable String filter,
-                                       @PathVariable String extra) {
-        if (userType == UserType.ADMIN) {
-            return taskService.getTasksByFilter(filter, extra);
-        }
-        throw new RuntimeException("No tienes permisos para filtrar tareas");
+        return userService.createUser(id,username, type);
     }
 }
+
--- src/main/java/edu/dosw/services/UserService.java
+++ src/main/java/edu/dosw/services/UserService.java
@@ -1,4 +1,30 @@
 package edu.dosw.services;
 
+import edu.dosw.model.User;
+import edu.dosw.model.UserType;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@Service
 public class UserService {
+
+    private final UserFactory userFactory;
+    private final Map<String, User> users = new HashMap<>();
+
+    public UserService(UserFactory userFactory) {
+        this.userFactory = userFactory;
+    }
+
+    public User createUser(String id, String username, UserType type) {
+        User user = userFactory.generateUser(id, username, type);
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    public Optional<User> getUserById(String id) {
+        return Optional.ofNullable(users.get(id));
+    }
 }
