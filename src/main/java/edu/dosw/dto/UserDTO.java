7bae9b1e12b42b55a5d90a0c973947523b398009
Sofia <Sofia>
2025-09-28 16:19:40 -0500

    refactor(user): Implement user persistence
    Refactors user creation and retrieval logic.
    - Uses UserDTO for repository interactions.
    - Implements UserFactory for user creation.
    - Adds username uniqueness validation.

--- src/main/java/edu/dosw/dto/UserDTO.java
+++ src/main/java/edu/dosw/dto/UserDTO.java
@@ -1,8 +1,11 @@
 package edu.dosw.dto;
 
-
 public record UserDTO (
+        String id,
         String username,
         String type
-){
+) {
+    public UserDTO(String username, String type) {
+        this(null, username, type);
+    }
 }
